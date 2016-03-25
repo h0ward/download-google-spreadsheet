dfaed7a6e1b07e91601e7fad683badf7e70c219c
h0ward <h0ward@outlook.com>
2016-03-25 22:53:00 +0800

    Setup the interface and load .env

--- src/bin/download-google-spreadsheet.js
+++ src/bin/download-google-spreadsheet.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-import '../index';
+import downloadGoogleSpreadsheet from '../index';
+
+const argv = process.argv.slice(2);
+
+downloadGoogleSpreadsheet.call(null, ...argv.map(JSON.parse), (err) => {
+  if (err) throw err;
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import {R_OK, accessSync} from 'fs';
+import dotenv from 'dotenv';
+
+export default function downloadGoogleSpreadsheet (sheets, ...args) {
+
+  // setup opts and cb
+  if (args.length === 1) args.unshift({});
+  let [opts, cb] = args;
+  if (!opts || Object.keys(opts).length === 0) {
+    try {
+      accessSync('.env', R_OK);
+      dotenv.config();
+    } catch (err) {}
+    opts = process.env;
+  }
+
+  // main logics
+  console.log('Hello world!');
+
+  // continue
+  cb(null);
+}
