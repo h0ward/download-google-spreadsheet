77eacf084f7b57c834c0ceaa462b724efbb70722
h0ward <h0ward@outlook.com>
2016-03-25 21:49:28 +0800

    Install gulp-sync and fix bin permission

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,23 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
+import babel from 'gulp-babel';
+import createSync from 'gulp-sync';
+import gulp from 'gulp';
+import {spawn} from 'child_process';
 
-gulp.task('default', () => {
+const {sync} = createSync(gulp);
+
+gulp.task('chmod', (cb) => {
+  spawn('chmod', ['--verbose', '+x', './bin/download-google-spreadsheet.js'], {stdio: 'inherit'})
+    .on('close', cb);
+});
+
+gulp.task('compile', (cb) => {
   gulp.src('src/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('.'))
+    .on('end', cb);
 });
+
+gulp.task('default', sync([
+  'compile',
+  'chmod',
+]));
